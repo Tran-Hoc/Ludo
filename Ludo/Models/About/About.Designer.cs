3b05d2a9816df6fd14fd645592008463c87b841d
Hoàng Minh Tài <hoangminhtai2810@gmail.com>
2022-05-15 11:22:04 +0700

    add teammate into About.cs

--- Ludo/Models/About/About.Designer.cs
+++ Ludo/Models/About/About.Designer.cs
@@ -32,6 +32,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label2
@@ -44,7 +53,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(131, 31);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Nhóm:  6";
+            this.label2.Text = "Nhóm:  5";
             // 
             // label3
             // 
@@ -52,11 +61,11 @@
             this.label3.BackColor = System.Drawing.Color.DimGray;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(46, 87);
+            this.label3.Location = new System.Drawing.Point(78, 89);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(175, 31);
+            this.label3.Size = new System.Drawing.Size(183, 31);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Thành viên: ";
+            this.label3.Text = "Thành viên:  ";
             // 
             // label1
             // 
@@ -64,11 +73,119 @@
             this.label1.BackColor = System.Drawing.Color.DimGray;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(46, 150);
+            this.label1.Location = new System.Drawing.Point(118, 133);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(182, 31);
+            this.label1.Size = new System.Drawing.Size(368, 31);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Trương Được";
+            this.label1.Text = "Trương Được - 6051071033";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DimGray;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(118, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(404, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Hoàng Minh Tài - 6051071102";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.DimGray;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(118, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(379, 31);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Trần Viết Học - 6051071048";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.DimGray;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(118, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(402, 31);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Võ Thành Nhân - 6051071081";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.DimGray;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(118, 414);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(461, 31);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Nguyễn Hoàng Hiệp - 6051071147";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.DimGray;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(118, 320);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(466, 31);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Cao Lâm Bảo Khanh - 6051071056";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.DimGray;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(118, 365);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(395, 31);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Nguyễn Hữu Đại 6051071023";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.DimGray;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(118, 552);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(440, 31);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Nguyễn Tuấn Khôi - 6051071060";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.DimGray;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(118, 505);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(483, 31);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "Nguyễn Thanh Dương - 6051071022";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.DimGray;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(118, 459);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(381, 31);
+            this.label13.TabIndex = 9;
+            this.label13.Text = "Bạch Long Vũ - 6051071145";
             // 
             // About
             // 
@@ -76,7 +193,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(499, 505);
+            this.ClientSize = new System.Drawing.Size(692, 611);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -93,5 +219,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+
     }
 }
